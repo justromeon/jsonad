ec230d911972deaed391e098dc29a48181e11ca9
Justin Kian Romeon <romeonjustin@gmail.com>
2025-03-02 00:18:01 +0800

    initial commit

---
+++ src/JSONParser.hs
@@ -0,0 +1,55 @@
+{-# LANGUAGE DeriveGeneric, LambdaCase #-}
+{-# LANGUAGE InstanceSigs #-}
+module JSONParser where
+
+import Data.Char (ord)
+import Data.List (intercalate)
+import GHC.Generics (Generic)
+import Numeric (showHex)
+
+data JValue = JNull
+            | JBool Bool
+            | JString String
+            | JNumber { int :: Integer, frac :: [Int], exponent :: Integer }
+            | JArray [JValue]
+            | JObject [(String, JValue)]
+            deriving (Eq, Generic)
+
+instance Show JValue where
+    show :: JValue -> String
+    show = \case
+      JNull          -> "null"
+      JBool True     -> "true"
+      JBool False    -> "false"
+      JString s      -> showJSONString s
+      JNumber i [] 0 -> show i
+      JNumber i f  0 -> show i ++ "." ++ concatMap show f
+      JNumber i [] e -> show i ++ "e" ++ show e
+      JNumber i f  e -> show i ++ "." ++ concatMap show f ++ "e" ++ show e
+      JArray a       -> "[" ++ intercalate ", " (map show a)     ++ "]"
+      JObject o      -> "{" ++ intercalate ", " (map showPair o) ++ "}"
+      where
+        showPair (k,v) = showJSONString k ++ ": " ++ show v
+
+showJSONString :: String -> String
+showJSONString s = "\"" ++ concatMap showJSONChar s ++ "\""
+
+isControl :: Char -> Bool
+isControl c = c `elem` ['\0' .. '\31']
+
+showJSONChar :: Char -> String
+showJSONChar c = case c of
+  '\'' -> "'"
+  '\"' -> "\\\""
+  '\\' -> "\\\\"
+  '/'  -> "\\/"
+  '\b' -> "\\b"
+  '\f' -> "\\f"
+  '\n' -> "\\n"
+  '\r' -> "\\r"
+  '\t' -> "\\t"
+  _ | isControl c -> "\\u" ++ showJSONNonASCIIChar c
+  _ -> [c]
+  where
+    showJSONNonASCIIChar x =
+      let a = "0000" ++ showHex (ord x) "" in drop (length a - 4) a
