a8bc43cfa26392d45d2f98262fceb2d6dfb2b272
Justin Kian Romeon <romeonjustin@gmail.com>
2025-05-11 23:18:32 +0800

    feat: Define core data types for JSON parser

--- src/JsonParser.hs
+++ src/JsonParser.hs
@@ -1,1 +1,11 @@
 module JsonParser where
+
+data JValue = JNull
+            | JBool Bool
+            | JNumber {int :: Integer, frac :: [Int], exponent :: Integer}
+            | JString String
+            | JArray [JValue]
+            | JObject [(String,JValue)]
+            deriving Show
+
+newtype Parser a = Parser { runP :: String -> Maybe (String, a) }
