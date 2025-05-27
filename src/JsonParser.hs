ddbb8b7ac45ed8fb9f191990f3796af2af5db226
Justin Kian Romeon <romeonjustin@gmail.com>
2025-05-27 19:25:56 +0800

    feat: Add JSON null and booleans parsers

--- src/JsonParser.hs
+++ src/JsonParser.hs
@@ -1,5 +1,9 @@
 module JsonParser where
 
+import Control.Applicative
+
+import Parse
+
 data JValue = JNull
             | JBool Bool
             | JNumber {int :: Integer, frac :: [Int], exponent :: Integer}
@@ -7,3 +11,9 @@ data JValue = JNull
             | JArray [JValue]
             | JObject [(String,JValue)]
             deriving Show
+
+jNull :: Parser JValue
+jNull = JNull <$ string "null"
+
+jBool :: Parser JValue
+jBool = JBool <$> ((False <$ string "false") <|> (True <$ string "true"))
