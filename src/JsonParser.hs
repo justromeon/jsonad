4f1e90f80d78e1bfe8c4a9e541e54f99721fd0f4
Justin Kian Romeon <romeonjustin@gmail.com>
2025-05-29 19:26:23 +0800

    feat: Add JSON number parser

--- src/JsonParser.hs
+++ src/JsonParser.hs
@@ -17,3 +17,22 @@ jNull = JNull <$ string "null"
 
 jBool :: Parser JValue
 jBool = JBool <$> ((False <$ string "false") <|> (True <$ string "true"))
+
+jInt :: Parser Double
+jInt = (0 <$ char '0') <|> (\n ns -> read (n:ns)) <$> nonZeroDig <*> digits
+
+jFrac :: Parser Double
+jFrac = read . ("0." ++) <$> (char '.' *> digits1) <|> pure 0
+
+jExp :: Parser Double
+jExp = oneOf "eE" *> (sign <*> (read <$> digits1)) <|> pure 0
+  where
+    sign = (negate <$ char '-') <|> (id <$ char '+') <|> pure id
+
+jNumber :: Parser JValue
+jNumber = do
+    sign <- (negate <$ char '-') <|> pure id
+    int  <- jInt
+    frac <- jFrac
+    expo <- jExp
+    pure $ JNumber $ sign $ (int + frac) * 10 ** expo
